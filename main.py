b5aeb81e4815da56ad5bb2097bae62ec35af7f3d
Nicholas Porter <nickporter94@gmail.com>
2023-09-24 22:22:41 -0400

    GUI update and goofin with look and buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 
-
+import os
 #Formatting the classlist to remove symbols and class numbers
 with open("classList.txt", "r") as file:
     rawclasslist = file.readlines()
@@ -19,35 +19,90 @@ for each in classlist:
 
 root = Tk()
 root.title("This is my name")
-root.maxsize(800, 600)
+root.maxsize(1800, 600)
+root.configure(background="red")
 
-frma = ttk.LabelFrame(root, text="Not Started", labelanchor="n")
-frmb = ttk.LabelFrame(root, text="Currently In", labelanchor="n")
-frmc = ttk.LabelFrame(root, text="Finished", labelanchor="n")
+# An absolute mess creating all the frames needed.
+frma = LabelFrame(root, text="Not Started", labelanchor="n", borderwidth=0, highlightcolor="gray")
+frmb = LabelFrame(root, text="Currently In", labelanchor="n", borderwidth=0)
+frmc = LabelFrame(root, text="Finished", labelanchor="n", borderwidth=0)
+frmbtna = LabelFrame(root, borderwidth=0, bg="red")
+frmbtnb = ttk.LabelFrame(root, borderwidth=0)
+frmbtnc = ttk.LabelFrame(root, borderwidth=0)
 
-frame_list = [frma, frmb, frmc]
-frma.grid(column=0)
-frmb.grid(column=1)
-frmc.grid(column=2)
+frma.grid(column=0, row=0)
+frmb.grid(column=1, row=0)
+frmc.grid(column=2, row=0)
+frmbtna.grid(column=0, row=1)
+frmbtnb.grid(column=1, row=1)
+frmbtnc.grid(column=2, row=1)
+#Creating a list of frames to remind me to refactor this shit later.
+frame_list = [frma, frmb, frmc, frmbtna, frmbtnb, frmbtnc]
 
 
-nsslist = Listbox(frma, width=word_width, height=25, selectmode=MULTIPLE)
-nslist = Text(frma, width=30, height=10)
+nsslist = Listbox(frma, width=word_width, height=25, selectmode=MULTIPLE, highlightthickness=0, borderwidth=0)
 nsslist.pack()
 
-#Not started list
-for each in classlist:
-    nsslist.insert(END, each)
+cinlist = Listbox(frmb, width=word_width, height=25,  selectmode=MULTIPLE, highlightthickness=0, borderwidth=0)
+cinlist.pack()
 
+finlist = Listbox(frmc, width=word_width, height=25, selectmode=MULTIPLE, highlightthickness=0, borderwidth=0)
+finlist.pack()
+
+#Not started list
+path = "./nsList.txt"
+if not os.path.exists(path):
+    with open("nsList.txt", "w") as file:
+        for each in classlistCOPY:
+            file.write(each + "\n")
+
+else:
+    with open("nsList.txt", "r") as file:
+        notstartlist = file.readlines()
+    
+    for each in notstartlist:
+        nsslist.insert(END, each)
+    
 #Currently in List
-for each in classlist:
-    nsslist.insert(END, each)
+path = "./cinList.txt"
+if not os.path.exists(path):
+    with open("cinList.txt", "w") as file:
+        file.close()
+with open("cinList.txt", "r") as file:
+    currentinlist = file.readlines()
+
+for each in currentinlist:
+    cinlist.insert(END, each)
 
 #Finished List
-for each in classlist:
-    nsslist.insert(END, each)
+path = "./finList.txt"
+if not os.path.exists(path):
+    with open("finList.txt", "w") as file:
+        file.close()
+with open("finList.txt", "r") as file:
+    finishedlist = file.readlines()
+for each in finishedlist:
+    finlist.insert(END, each)
 
-b = ttk.Button()
+
+def moveclassover(source, destination):
+    selected = source.curselection()
+    for each in selected[::-1]:
+        destination.insert(END, source.get(each))
+        source.delete(each)
 
 
+
+        
+
+img = PhotoImage(file="testimg.png")
+
+notstartedbtn = Button(frmbtna, command=lambda: moveclassover(nsslist, cinlist), image=img, width=20, height=10)
+notstartedbtn.grid(column=0, row=1, pady=10, padx=5)
+
+firstcinbtn = Button(root, command=lambda: moveclassover(cinlist, nsslist), image=img, width=20, height=10)
+firstcinbtn.grid(column=1, row=1, pady=10, padx=5)
+
+firstcinbtntwo = Button(root, command=lambda: moveclassover(cinlist, nsslist), image=img, width=20, height=10)
+firstcinbtntwo.grid(column=1, row=1, pady=10, padx=5)
 root.mainloop()
