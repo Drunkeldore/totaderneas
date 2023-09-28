5ea33b208d8b4edc2894b17e0bf9600e2478ae33
Nicholas Porter <nickporter94@gmail.com>
2023-09-27 21:10:03 -0400

    GUI update. Buttons have arrows. Working on save functionality

--- main.py
+++ main.py
@@ -26,22 +26,22 @@ root.configure(background="#1a5d7f")
 frma = LabelFrame(root, text="Not Started", labelanchor="n", borderwidth=0, bg="#003057", font=("FUTURA STANDARD", 16, "underline"), fg="white")
 frmb = LabelFrame(root, text="Currently In", labelanchor="n", borderwidth=0, bg="#003057", font=("FUTURA STANDARD", 16, "underline"), fg="white")
 frmc = LabelFrame(root, text="Finished", labelanchor="n", borderwidth=0, bg="#003057", font=("FUTURA STANDARD", 16, "underline"), fg="white")
-frmbtna = LabelFrame(root, borderwidth=0)
-frmbtnb = ttk.LabelFrame(root, borderwidth=0)
-frmbtnc = ttk.LabelFrame(root, borderwidth=0)
+# frmbtna = LabelFrame(root, borderwidth=0)
+# frmbtnb = ttk.LabelFrame(root, borderwidth=0)
+# frmbtnc = ttk.LabelFrame(root, borderwidth=0)
 
 frma.grid(column=0, row=0)
 frmb.grid(column=1, row=0)
 frmc.grid(column=2, row=0)
-frmbtna.grid(column=0, row=1)
-frmbtnb.grid(column=1, row=1)
-frmbtnc.grid(column=2, row=1)
+# frmbtna.grid(column=0, row=1)
+# frmbtnb.grid(column=1, row=1)
+# frmbtnc.grid(column=2, row=1)
 #Creating a list of frames to remind me to refactor this shit later.
-frame_list = [frma, frmb, frmc, frmbtna, frmbtnb, frmbtnc]
+#frame_list = [frma, frmb, frmc, frmbtna, frmbtnb, frmbtnc]
 
 
-nsslist = Listbox(frma, width=word_width, height=25, selectmode=MULTIPLE, highlightthickness=0, borderwidth=, font=("FUTURA STANDARD", 12))
-nsslist.pack()
+nslist = Listbox(frma, width=word_width, height=25, selectmode=MULTIPLE, highlightthickness=0, borderwidth=0, font=("FUTURA STANDARD", 12))
+nslist.pack()
 
 cinlist = Listbox(frmb, width=word_width, height=25,  selectmode=MULTIPLE, highlightthickness=0, borderwidth=0, font=("FUTURA STANDARD", 12))
 cinlist.pack()
@@ -61,7 +61,7 @@ else:
         notstartlist = file.readlines()
     
     for each in notstartlist:
-        nsslist.insert(END, each)
+        nslist.insert(END, each)
     
 #Currently in List
 path = "./cinList.txt"
@@ -90,21 +90,36 @@ def moveclassover(source, destination):
     for each in selected[::-1]:
         destination.insert(END, source.get(each))
         source.delete(each)
+    #print(nslist.get(0, 'end'))
+    with open("finList.txt", "w") as file:
+        for each in source.get(0, "end"):
+            file.write(each)
+
+#TODO refactor each list.txt to be all lowercase. Makes this function significantly easier to manage.
+
 
 
 
         
 
-img = PhotoImage(file="testimg.png")
 
-notstartedbtn = Button(root, command=lambda: moveclassover(nsslist, cinlist), image=img, width=20, height=10)
+
+#Movement Buttons
+rightarrow = PhotoImage(file="./images/rightArrow.png")
+leftarrow = PhotoImage(file="./images/leftArrow.png")
+
+notstartedbtn = Button(root, command=lambda: moveclassover(nslist, cinlist), image=rightarrow, width=20, height=10)
 notstartedbtn.grid(column=0, row=1, pady=10, padx=5)
 
-firstcinbtn = Button(root, command=lambda: moveclassover(cinlist, nsslist), image=img, width=20, height=10)
+firstcinbtn = Button(root, command=lambda: moveclassover(cinlist, finlist), image=rightarrow, width=20, height=10)
 firstcinbtn.grid(column=1, row=1, pady=10, padx=5)
 
-
-#How do I create multiple btns in same space of grid? 
-firstcinbtntwo = Button(root, command=lambda: moveclassover(cinlist, nsslist), image=img, width=20, height=10)
+firstcinbtntwo = Button(root, command=lambda: moveclassover(cinlist, nslist), image=leftarrow, width=20, height=10)
 firstcinbtntwo.grid(column=1, row=2, pady=10, padx=5)
+
+finbtn = Button(root, command=lambda: moveclassover(finlist, cinlist), image=leftarrow, width=20, height=20)
+finbtn.grid(column=2, row=1)
+
+
+
 root.mainloop()
